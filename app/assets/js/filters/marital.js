e0abbdaa4b68aea1971c2d2608882ca65f94ce22
Trea Hauet <trea@treahauet.com>
2013-11-05 04:42:47 -0700

    Fixing marital status translation filter

--- app/assets/js/filters/marital.js
+++ app/assets/js/filters/marital.js
@@ -2,27 +2,33 @@ angular.module('gmaApp.filters').filter('marital', function(){
 	return function(input, length) {
 		if (input != undefined) {
 			switch (input) {
+				case '0':
 				case 0:
 					return 'Never Married';
 				break;
 
 				case 1:
+				case '1':
 					return 'Separated';
 				break;
 
 				case 2:
+				case '2':
 					return 'Widowed';
 				break;
 
 				case 3:
+				case '3':
 					return "Divorced";
 				break;
 
 				case 4:
+				case '4':
 					return 'Remarried';
 				break;
 
 				case 5:
+				case '5':
 					return 'Married';
 				break;
 			}
