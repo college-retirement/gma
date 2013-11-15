bd92828b55649b408328332a7e3f33436d518348
Trea Hauet <trea@treahauet.com>
2013-11-15 05:01:11 -0700

    Fixes #3

--- app/assets/js/controllers/admin/adminEditCtrl.js
+++ app/assets/js/controllers/admin/adminEditCtrl.js
@@ -15,4 +15,64 @@ angular.module('gmaApp').controller('AdminEditCtrl', function($scope, $route, $h
 			});
 		}
 	});
+
+	// Defined types
+	$scope.ownershipTypes = OwnershipTypes;
+	$scope.liabilityTypes = LiabilityTypes;
+	$scope.assetTypes = AssetTypes;
+	$scope.retirementTypes = RetirementTypes;
+
+	$scope.addSchool = function() {
+		$scope.student.schools.push($scope.currentSchool);
+		$scope.currentSchool = null;
+	};
+
+	$scope.updateSchool = function(school, index) {
+		$scope.currentSchool = school;
+	};
+	
+	$scope.deleteSchool = function(index) {
+		$scope.student.schools.splice(index, 1);
+	};
+
+
+	$scope.addFamily = function() {
+		$scope.student.family.members.push({student: false});
+	};
+
+	$scope.deleteFamily = function(index) {
+		$scope.student.family.members.splice(index, 1);
+	}
+
+	$scope.addProperty = function() {
+		$scope.student.family.realEstate.push({});
+	};
+
+	$scope.deleteProperty = function(index) {
+		$scope.student.family.realEstate.splice(index, 1);
+	}
+
+	$scope.addAsset = function() {
+		$scope.student.family.assets.push({});
+	};
+
+	$scope.deleteAsset = function(index) {
+		$scope.student.family.assets.splice(index, 1);
+	}
+
+	$scope.addLiability = function() {
+		$scope.student.family.liabilities.push({});
+	};
+
+	$scope.deleteLiability = function(index) {
+		$scope.student.family.liabilities.splice(index, 1);
+	};
+
+	$scope.addRetirement = function() {
+		$scope.student.family.retirement.push({});
+	};
+
+	$scope.deleteRetirement = function(index) {
+		$scope.student.family.retirement.splice(index, 1);
+	};
 });
