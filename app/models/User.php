3257e700989fd0ac51ef412a47552efb19ee93bd
Trea Hauet <trea@treahauet.com>
2014-01-13 14:50:02 -0700

    Updated model

--- app/models/User.php
+++ app/models/User.php
@@ -4,113 +4,118 @@ use Jenssegers\Mongodb\Model as Eloquent;
 use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableInterface;
 
+class User extends Eloquent implements UserInterface, RemindableInterface
+{
 
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $collection = 'users';
 
-class User extends Eloquent implements UserInterface, RemindableInterface {
+    public $appends = array("full_name", "is_admin", "created", "updated");
 
-	/**
-	 * The database table used by the model.
-	 *
-	 * @var string
-	 */
-	protected $collection = 'users';
+    public function getCreatedAttribute()
+    {
+        if (array_key_exists('created_at', $this->attributes)) {
+            if (is_string($this->attributes['created_at'])) {
+                $dt = new DateTime($this->attributes['created_at']);
+                return $dt->format('c');
+            }
+            if (is_object($this->attributes['created_at'])) {
+                $dt = new DateTime();
+                $dt->setTimestamp($this->attributes['created_at']->sec);
+                return $dt->format('c');
+            }
+        }
+    }
 
-	public $appends = array("name", "is_admin", "created", "updated");
+    public function getUpdatedAttribute()
+    {
+        if (array_key_exists('updated_at', $this->attributes)) {
+            if (is_string($this->attributes['updated_at'])) {
+                $dt = new DateTime($this->attributes['updated_at']);
+                return $dt->format('c');
+            }
+            if (is_object($this->attributes['updated_at'])) {
+                $dt = new DateTime();
+                $dt->setTimestamp($this->attributes['updated_at']->sec);
+                return $dt->format('c');
+            }
+        }
+    }
 
-	function getCreatedAttribute() {
-		if (array_key_exists('created_at', $this->attributes)) {
-			if (is_string($this->attributes['created_at'])) {
-				$dt = new DateTime($this->attributes['created_at']);
-				return $dt->format('c');
-			}
-			if (is_object($this->attributes['created_at'])) {
-				$dt = new DateTime();
-				$dt->setTimestamp($this->attributes['created_at']->sec);
-				return $dt->format('c');
-			}
-		}
-	}
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = array();
 
-	function getUpdatedAttribute() {
-		if (array_key_exists('updated_at', $this->attributes)) {
-			if (is_string($this->attributes['updated_at'])) {
-				$dt = new DateTime($this->attributes['updated_at']);
-				return $dt->format('c');
-			}
-			if (is_object($this->attributes['updated_at'])) {
-				$dt = new DateTime();
-				$dt->setTimestamp($this->attributes['updated_at']->sec);
-				return $dt->format('c');
-			}
-		}	
-	}
+    /**
+     * Get the unique identifier for the user.
+     *
+     * @return mixed
+     */
+    public function getAuthIdentifier()
+    {
+        return $this->getKey();
+    }
 
-	/**
-	 * The attributes excluded from the model's JSON form.
-	 *
-	 * @var array
-	 */
-	protected $hidden = array();
+    /**
+     * Get the password for the user.
+     *
+     * @return string
+     */
+    public function getAuthPassword()
+    {
+        return $this->password;
+    }
 
-	/**
-	 * Get the unique identifier for the user.
-	 *
-	 * @return mixed
-	 */
-	public function getAuthIdentifier()
-	{
-		return $this->getKey();
-	}
+    /**
+     * Get the e-mail address where password reminders are sent.
+     *
+     * @return string
+     */
+    public function getReminderEmail()
+    {
+        return $this->email;
+    }
 
-	/**
-	 * Get the password for the user.
-	 *
-	 * @return string
-	 */
-	public function getAuthPassword()
-	{
-		return $this->password;
-	}
+    public function drafts()
+    {
+        return $this->hasMany('Draft');
+    }
 
-	/**
-	 * Get the e-mail address where password reminders are sent.
-	 *
-	 * @return string
-	 */
-	public function getReminderEmail()
-	{
-		return $this->email;
-	}
+    public function profiles()
+    {
+        return $this->hasMany('Profile');
+    }
 
-	public function drafts() {
-		return $this->hasMany('Draft');
-	}
+    public function getFullNameAttribute()
+    {
+        if (array_key_exists('name', $this->attributes)) {
+            return $this->attributes['name']['first'] . ' ' . $this->attributes['name']['last'];
+        } else {
+            return $this->attributes['email'];
+        }
+    }
 
-	public function profiles() {
-		return $this->hasMany('Profile');
-	}
+    public function getIsAdminAttribute()
+    {
+        if (array_key_exists('role', $this->attributes)) {
+            return $this->attributes['role'] == 'Administrator';
+        }
+        return false;
+    }
 
-	public function getNameAttribute() {
-		if (array_key_exists('name', $this->attributes)) {
-			return $this->attributes['name']['first'] . ' ' . $this->attributes['name']['last'];
-		}
-		else {
-			return $this->attributes['email'];
-		}
-	}
+    public static function boot()
+    {
+        parent::boot();
 
-	public function getIsAdminAttribute() {
-		if (array_key_exists('role', $this->attributes)) {
-			return $this->attributes['role'] == 'Administrator';
-		}
-		return false;
-	}
-
-	public static function boot() {
-		parent::boot();
-
-		static::created(function($user){
-			Event::fire('user.create', [$user]);
-		});
-	}
+        static::created(function ($user) {
+            Event::fire('user.create', [$user]);
+        });
+    }
 }
