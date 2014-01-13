97a5a493afe997982db0fd7aec58e1eadc264dbf
Trea Hauet <trea@treahauet.com>
2014-01-13 10:23:45 -0700

    Sort Draft List

--- app/assets/js/controllers/admin/drafts/adminList.js
+++ app/assets/js/controllers/admin/drafts/adminList.js
@@ -6,16 +6,72 @@ angular.module('gmaApp').controller('AdminDraftListCtrl', function($scope, Perso
 		$scope.pagination = obj.data.pagination;
 	});
 
+	$scope.sortable = {
+		'user.name.last': false,
+		'user.name.first': false,
+		'name.last': false,
+		'name.first': false,
+		'created_at': false,
+		'updated_at': false
+	};
+
+	$scope.checkSort = function (column) {
+		return $scope.sortable[column];
+	};
+
+	$scope.sortClass = function (column) {
+		var direction = $scope.sortable[column];
+
+		if (direction === false) {
+			return;
+		}
+		else if (direction == 'asc') {
+			return 'icon-caret-up';
+		}
+		else if (direction == 'desc') {
+			return 'icon-caret-down';
+		}
+	};
+
+	$scope.changeSort = function (column) {
+		var current = $scope.sortable[column];
+
+		if (current === false) {
+			$scope.sortable[column] = 'asc';
+		} else if (current == 'desc') {
+			$scope.sortable[column] = 'asc';
+		} else if (current == 'asc') {
+			$scope.sortable[column] = 'desc';
+		}
+
+		$scope.newPage(1);
+	};
+
+	$scope.clearSort = function (column) {
+		$scope.sortable[column] = false;
+		$scope.newPage(1);
+	};
+
 	$scope.viewDraft = function(profile) {
 		$location.path('/admin/drafts/' + profile._id);
-	}
+	};
 
 	$scope.newPage = function(page) {
 		toastr.info('', 'Loading', {
 			timeOut: 0,
 			extendedTimeOut: 0,
 		});
-		$http.get('/admin/drafts?page=' + page).then(function(obj){
+
+		var sort = [];
+
+		for (var key in $scope.sortable) {
+			var sortCol = $scope.sortable[key];
+			if (sortCol == 'asc' || sortCol == 'desc') {
+				sort.push(key + ':' + sortCol);
+			}
+		}
+
+		$http.get('/admin/drafts?page=' + page + '&sort=' + sort.join(',')).then(function(obj){
 			if(!$scope.$$phase) {
 				$scope.$digest(function(){
 					$scope.profiles = obj.data.result;
@@ -28,5 +84,5 @@ angular.module('gmaApp').controller('AdminDraftListCtrl', function($scope, Perso
 			}
 			toastr.clear();
 		});
-	}
+	};
 });
