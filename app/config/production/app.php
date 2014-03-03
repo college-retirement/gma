d25d2c5a42e21a9faacb07a21c1b7463d8701cbc
Trea Hauet <trea@treahauet.com>
2014-03-03 14:37:45 -0700

    Production config

---
+++ app/config/production/app.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+	
+	//Don't show exception messages in production
+	'debug' => FALSE,
+
+	// Set production security key (used in encryption and authentication processes)
+	'key' => 'EwXnBN7LYptXYUr5qRKT9P0IGJrRFQ7T'
+);
