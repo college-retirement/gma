355c3a5f91825bb4af08acadab052dff642d4675
Linode Server <sean@zimplifyit.com>
2014-06-10 14:30:03 +0000

    autocommit on Tue Jun 10 14:30:03 UTC 2014

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -29,7 +29,7 @@ $app->redirectIfTrailingSlash();
 $env = $app->detectEnvironment(array(
 
 	'local' => ['gma.dev'],
-	'production' => ['clients.college-retirement.com'],
+	'production' => ['dev.college-retirement.com'],
 	'staging' => ['crs.trea.ws']
 
 ));
