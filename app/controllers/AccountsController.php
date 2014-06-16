44372e4da4c501cf7a63a5d7a70099053eb4cea5
Linode Server <sean@zimplifyit.com>
2014-06-16 17:00:02 +0000

    autocommit on Mon Jun 16 17:00:02 UTC 2014

--- app/controllers/AccountsController.php
+++ app/controllers/AccountsController.php
@@ -20,79 +20,35 @@ class AccountsController extends Controller {
 
 			if ($user->save()) {
 				return Response::json($user, 201);
-
 			}
 			else {
 				return Response::json(['messages' => ['db_error' => 'Unable to create account.']], 500);
 			}
 		}
 	}
+
+	/**
+	* as we are using previous version of enssegers/mongodb for that reasone we can not use password reminder 
+	*  so we are using manual password recovery
+	*/
 	function forgot() {
 		$user = User::where('email', Input::get('email'))->get()->first();
 		if ($user) {
 			
 			$data['email'] = $user->email;
-			$reminder = new Reminder;
-			$reminder->email = $user->email;
-			$reminder->token = Hash::make(time());
-			$data['token'] = $reminder->token;
-
-			if($reminder->save()){
-				
-				Mail::send('emails.auth.reminder', $data, function($message) use ($reminder)
-				{
-				    $message->from('info@college-retirement.com', 'Dev');
-
-				    $message->to($reminder->email);
-
-				    
-				});
-				return Response::json(['message' => 'success'], 201);
-			}
-			//return Password::remind($data);
+			return Password::remind($data);
 		}
 		else{
 			return Response::json(['messages' => ['no_email' => 'There is no account with this email']], 409);
 		}
 	}
 
-	function reset() {
-
-		$rules = array(
-                'token' => 'Required|Min:10',
-                'password'  =>'Required|Confirmed',
-                'password_confirmation'=>'Required'
-        );
-
-         $validator = Validator::make(Input::all(), $rules);
-         
-	    if ($validator->fails())
-	    {
-	    	return Response::json(['messages' => ['error' => ['resone' =>  'password does not match']]], 409);
-
-	    } else { 
-
-		$reminder = Reminder::where('token',Input::get('token'))->get()->first();
-		if($reminder){
-			$user = User::where('email', $reminder->email)->get()->first();
-			$user->password = Hash::make(Input::get('password'));
-			if ($user->save()) {
-				$reminder->delete();
-				return Response::json(['message' => 'success'], 201);
-
-			}
-			else {
-				return Response::json(['messages' => ['db_error' => 'Unable to update account.']], 500);
-			}
-		}
-	}
-	}
-
 	function personaVerify() {
 		$data = Input::all();
 		
-		$email = $data[0]['value'];
-		$password = $data[1]['value'];
+		$email = Input::get('email');
+		$password = Input::get('password');
+
 		//$user = User::find('53998c05048321d65d0104d9');
 		$user = User::where('email', $email)->get()->first();
 		//$user = User::where('email', '=','mahfuz@gmail.com');
