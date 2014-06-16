f30eb65b75797c6cc315c81de7d26fd6915a0438
Linode Server <sean@zimplifyit.com>
2014-06-16 17:30:02 +0000

    autocommit on Mon Jun 16 17:30:02 UTC 2014

--- app/models/Profile.php
+++ app/models/Profile.php
@@ -10,7 +10,7 @@ class Profile extends Eloquent
 
     public function scopeProspect($query)
     {
-        return $query->where('prospect', true)->orWhereRaw(['prospect' => ['$exists' => false]]);
+        return $query->where('prospect', true)->orWhereRaw(['prospect' => ['$exists' => false]])->orderBy('updated_at', 'desc');
     }
 
     public function scopeClient($query)
