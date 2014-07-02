c90a899f1b0a8457e289f4bde6bb63dcef87df20
Linode Server <sean@zimplifyit.com>
2014-07-02 21:30:03 +0000

    autocommit on Wed Jul  2 21:30:03 UTC 2014

--- app/lib/GMA/Data/Models/Log.php
+++ app/lib/GMA/Data/Models/Log.php
@@ -36,12 +36,12 @@ class Log extends SortableModel {
         if (array_key_exists('updated_at', $this->attributes)) {
             if (is_string($this->attributes['updated_at'])) {
                 $dt = new DateTime($this->attributes['updated_at']);
-                return $dt->format('c');
+                return 'st'.$dt->format( 'd-m-Y' );
             }
             if (is_object($this->attributes['updated_at'])) {
                 $dt = new DateTime();
                 $dt->setTimestamp($this->attributes['updated_at']->sec);
-                return $dt->format('c');
+                return 'ob'.$dt->format('c');
             }
         }
     }
