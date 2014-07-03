4025b7e8859d96073f2629fe2385a724bcac5d3f
Linode Server <sean@zimplifyit.com>
2014-07-03 22:00:08 +0000

    autocommit on Thu Jul  3 22:00:08 UTC 2014

--- app/lib/GMA/Data/Models/Profile.php
+++ app/lib/GMA/Data/Models/Profile.php
@@ -40,12 +40,12 @@ class Profile extends SortableModel
         if (array_key_exists('created_at', $this->attributes)) {
             if (is_string($this->attributes['created_at'])) {
                 $dt = new DateTime($this->attributes['created_at']);
-                return $dt->format('c');
+                return $dt->format('M d, Y');
             }
             if (is_object($this->attributes['created_at'])) {
                 $dt = new DateTime();
                 $dt->setTimestamp($this->attributes['created_at']->sec);
-                return $dt->format('c');
+                return $dt->format('M d, Y');
             }
         }
     }
@@ -55,12 +55,12 @@ class Profile extends SortableModel
         if (array_key_exists('updated_at', $this->attributes)) {
             if (is_string($this->attributes['updated_at'])) {
                 $dt = new DateTime($this->attributes['updated_at']);
-                return $dt->format('c');
+                return $dt->format('M d, Y');
             }
             if (is_object($this->attributes['updated_at'])) {
                 $dt = new DateTime();
                 $dt->setTimestamp($this->attributes['updated_at']->sec);
-                return $dt->format('c');
+                return $dt->format('M d, Y');
             }
         }
     }
