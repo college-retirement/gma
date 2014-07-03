fce693532d9089948af57f1d3cd466f8bf58691a
Linode Server <sean@zimplifyit.com>
2014-07-03 18:00:02 +0000

    autocommit on Thu Jul  3 18:00:02 UTC 2014

--- app/lib/GMA/Controllers/Admin/UserController.php
+++ app/lib/GMA/Controllers/Admin/UserController.php
@@ -13,7 +13,10 @@ class UserController extends Base
             $query = User::with(['drafts', 'profiles'])->withSortables($this->sortableColumns())->paginate(30);
             return Rest::okay($query);
         } else {
-            return Rest::okay(User::with(['drafts', 'profiles'])->simplePaginate(30));
+            $data = User::all();
+            $d = \Illuminate\Pagination::make($data,count($data),10);
+            return Rest::okay($d);
+            //return Rest::okay(User::with(['drafts', 'profiles'])->paginate(30));
         }
     }
 }
