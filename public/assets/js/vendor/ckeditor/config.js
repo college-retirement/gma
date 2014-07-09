30bb60e0d629bfc6e635ab4dc231c554441537af
Linode Server <sean@zimplifyit.com>
2014-07-09 20:00:01 +0000

    autocommit on Wed Jul  9 20:00:01 UTC 2014

--- public/assets/js/vendor/ckeditor/config.js
+++ public/assets/js/vendor/ckeditor/config.js
@@ -7,6 +7,7 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here.
 	// For the complete reference:
 	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
+	config.height = '450px';
 
 	// The toolbar groups arrangement, optimized for two toolbar rows.
 	config.toolbarGroups = [
