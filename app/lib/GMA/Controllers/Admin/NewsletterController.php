6ae4aba2bcbc2367bf2d859de6647d5c147d0e99
Linode Server <sean@zimplifyit.com>
2014-07-15 21:30:01 +0000

    autocommit on Tue Jul 15 21:30:01 UTC 2014

--- app/lib/GMA/Controllers/Admin/NewsletterController.php
+++ app/lib/GMA/Controllers/Admin/NewsletterController.php
@@ -56,67 +56,84 @@ class NewsletterController extends Base {
         // {WorkPhone2}
         // {BackEndScheduleLink}
 
-        $profile = Profile::client()->with('user')->where("_id" , $id)->get()->first();
-        
-            $template['ID'] = $profile['_id'] ;
-            $template['CaseID'] = $profile['client_id'];
-            $template['StudentFName'] = $profile['name']['first'] ;
-            $template['StudentLName'] = $profile['name']['last'];
+        $profile = Profile::client()->where("_id" , $id)->get()->first();
+        var_dump($profile);die();
+            $variables['ID'] = $profile['_id'] ;
+            $variables['CaseID'] = $profile['client_id'];
+            $variables['StudentFName'] = $profile['name']['first'] ;
+            $variables['StudentLName'] = $profile['name']['last'];
             if (isset($profile['parents']['father'])) {
-                $template['ParentFName'] = $profile['parents']['father']['name']['first'];
-                $template['ParentLName'] = $profile['parents']['father']['name']['first'];
+                $variables['ParentFName'] = $profile['parents']['father']['name']['first'];
+                $variables['ParentLName'] = $profile['parents']['father']['name']['first'];
             } elseif (isset($profile['parents']['mother'])) {
-                $template['ParentFName'] = $profile['parents']['mother']['name']['first'];
-                $template['ParentLName'] = $profile['parents']['mother']['name']['first'];
+                $variables['ParentFName'] = $profile['parents']['mother']['name']['first'];
+                $variables['ParentLName'] = $profile['parents']['mother']['name']['first'];
             } else {
-                $template['ParentFName'] = "";
-                $template['ParentLName'] = "";
+                $variables['ParentFName'] = "";
+                $variables['ParentLName'] = "";
             }
-            $template['Address'] =  $profile['address']['line1'];
-            $template['City'] = $profile['address']['city'];
-            $template['State'] = $profile['address']['state'];
-            $template['Zip'] = $profile['address']['zipcode'];
-            $template['EmailAddress'] = $profile['email'] ;
-            $template['GradYear'] = $profile['hsGrad'];
-            $template['HighSchool'] = '';
+            $variables['Address'] =  $profile['address']['line1'];
+            $variables['City'] = $profile['address']['city'];
+            $variables['State'] = $profile['address']['state'];
+            $variables['Zip'] = $profile['address']['zipcode'];
+            $variables['EmailAddress'] = $profile['email'] ;
+            $variables['GradYear'] = $profile['hsGrad'];
+            $variables['HighSchool'] = '';
             if (isset($profile['school']) && isset($profile['school']['name']))
-                $template['HighSchool'] = $profile['school']['name'];
-             $template['HomePhone'] = $profile['phone'];
+                $variables['HighSchool'] = $profile['school']['name'];
+             $variables['HomePhone'] = $profile['phone'];
 
              //rest of the template are unknown now so just initialize it
 
-             $template['ApptDate'] = '';
-             $template['ApptLocation'] = '';
-             $template['ApptGoToMeetingID'] = '';
-             $template['Password'] ='';
-             $template['DataformLink'] = '';
-             $template['LearningStyleLink'] = '';
-             $template['WebinarDate'] = '';
-             $template['WebinarName'] = '';
-             $template['WebinarGoToMeetingID'] = '';
-             $template['WorkPhone1'] = '';
-             $template['WorkPhone2'] = '';
-             $template['BackEndScheduleLink'] = '';
+             $variables['ApptDate'] = '';
+             $variables['ApptLocation'] = '';
+             $variables['ApptGoToMeetingID'] = '';
+             $variables['Password'] ='';
+             $variables['DataformLink'] = '';
+             $variables['LearningStyleLink'] = '';
+             $variables['WebinarDate'] = '';
+             $variables['WebinarName'] = '';
+             $variables['WebinarGoToMeetingID'] = '';
+             $variables['WorkPhone1'] = '';
+             $variables['WorkPhone2'] = '';
+             $variables['BackEndScheduleLink'] = '';
 
-              $query = Template::withSortables($this->sortableColumns())->paginate(2);
-
-              foreach ($query as $key => $value) {
-                 var_dump($value);
-              }
-        
-            die();
-
-        if ($this->isSorting()) {
+              
+         var_dump($variables);die();
             
-            $query = Template::withSortables($this->sortableColumns())->paginate(70);
-            return Rest::okay($query);
-        } else {
 
             $sortableColumns = [['column' => 'templateType',
                     'order' => 'DESC']];
                     
-            $templates = Template::withSortables($sortableColumns)->paginate(70);
+            $query= Template::withSortables($sortableColumns)->paginate(70);
+
+            foreach ($query as $key => $value) {
+                 $templates[] = [
+                        '_id' => $value['_id'],
+                        'created' => $value['created'],
+                        'isMasterTemplate' => $value['isMasterTemplate'],
+                        'templateBody' => $this->parsetemplate($variables, $value['templateBody']),
+                        'templateID' => $value['templateID'],
+                        'templateName' => $value['templateName'],
+                        'templateSubject' => $value['templateSubject'],
+                        'templateType' => $value['templateType'],
+                        'updated' => $value['updated'],
+                        'updated_at' => $value['updated_at'],
+                        'useMasterTemplate' => $value['useMasterTemplate']
+                    ];
+              }
             return Rest::okay($templates);
+        
+    }
+
+    public function parsetemplate($variables, $template)
+    {
+        var_dump($variables);die();
+        foreach($variables as $key => $val)
+        {
+            $template = str_replace('{'.$key.'}', $val, $template);
         }
+
+        return $template;
     }
 }
